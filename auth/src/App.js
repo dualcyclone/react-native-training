c9c2e25a9f6e92206d812516fb7ff25290cafaa3
Luke Dyson <luke.dyson@flyinganvil.co.uk>
2017-03-15 08:44:55 +0000

    Lessons 74-76 of https://www.udemy.com/the-complete-react-native-and-redux-course/learn/v4/overview
    The sign out and spinner buttons are displayed behind the header - need to understand why this is, for now, I've wrapped in the Card/CardSection components to help alleviate this

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import firebase from 'firebase';
+import firebase, { auth } from 'firebase';
 
-import { Header } from './components/common';
+import { Card, CardSection, Button, Header, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   // Executed before render time
   componentWillMount() {
     // Initialize Firebase
@@ -17,15 +19,44 @@ class App extends Component {
       storageBucket: "authentication-aa6a0.appspot.com",
       messagingSenderId: "105026534415"
     });
+
+    auth().onAuthStateChanged((user) => {
+      this.setState({ loggedIn: !!user });
+    });
   }
 
   render() {
     return (
       <View>
         <Header title="Authentication" />
-        <LoginForm />
+        { this.renderForm() }
       </View>
     );
+  }
+
+  renderForm() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          // Displays behind the header... so I've wrapped in these for now
+          <Card>
+            <CardSection>
+              <Button onPress={ () => auth().signOut() }>Log Out</Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          // Displays behind the header... so I've wrapped in these for now
+          <Card>
+            <CardSection>
+              <Spinner size="large" />
+            </CardSection>
+          </Card>
+        );
+    }
   }
 }
 
